4c470edfba2c2715c0a29f091194271879aba52d
N3ptune <100999898+T4t4KAU@users.noreply.github.com>
2022-04-04 18:06:50 +0800

    Add files via upload

---
+++ linux_ipc/shmA.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <signal.h>
+
+int* p = NULL;
+
+void hand()
+{
+    // 卸载共享内存
+    shmdt(p);
+    printf("bye\n");
+    exit(0);
+}
+
+int main(void)
+{
+    signal(2,hand);
+    // 创建key
+    key_t key = ftok(".",'m');
+    if (-1 == key) {
+        printf("ftok error:%m\n");
+        exit(-1);
+    }
+    printf("ftok success\n");
+    // 获取共享内存
+    int shmid = shmget(key,4096,IPC_CREAT);
+    if (-1 == shmid) {
+        printf("shmid error:%m\n");
+        exit(-1);
+    }
+    // 挂载共享内存
+    p = (int*)shmat(shmid,NULL,0);
+    if ((int*)-1 == p) {
+        printf("shmat error:%m\n");
+        exit(-1);
+    }
+    printf("shmat success\n");
+    // 使用共享内存
+    int n = 0;
+    while(true) {
+        *p = n++;
+        printf("%d\n",*p);
+        sleep(1);
+    } 
+    return 0;
+}
